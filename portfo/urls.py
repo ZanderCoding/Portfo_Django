c8b561c5b9632ec0d9912bbd699d05f01e5b3ecf
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-11-15 11:07:29 -0800

    Added Basic Project page

--- portfo/urls.py
+++ portfo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, about, skill_view, contact_view, SkillDetailView, contact_success_view
+from .views import HomeView, about, skill_view, contact_view, SkillDetailView, contact_success_view, projects_page
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', about, name='about'),
     path('skill/', skill_view, name='skill'),
+    path('projects/', projects_page, name='projects-page'),
     path('contact/', contact_view, name='contact'),
     path('contact/success/', contact_success_view, name='contact-sucess'),
     path('skill/<int:pk>/', SkillDetailView.as_view(), name='skill-detail'),
