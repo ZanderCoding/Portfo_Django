fe666126e35dbb9a62e7ad8a2263267c77b567b0
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-11-14 19:55:13 -0800

    Added basic contact page

--- portfo/models.py
+++ portfo/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-# Create your models here.
 
 
 class Skill(models.Model):
@@ -25,3 +22,13 @@ class Skill(models.Model):
 
     def __str__(self):
         return f"{self.name} | {self.field}"
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
