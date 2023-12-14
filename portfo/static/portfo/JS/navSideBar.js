b04d86274675406cf622e89770394529eb6fad9d
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-12-14 15:19:11 -0800

    Now able to click of nav bar on smaller screens

--- portfo/static/portfo/JS/navSideBar.js
+++ portfo/static/portfo/JS/navSideBar.js
@@ -1,14 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.querySelector('.menu-toggle');
     const navbar = document.querySelector('.navbar');
-    const darkModeSwitch = document.getElementById('dark-mode-toggle');
+
+    document.addEventListener('click', function (event) {
+        const isOpen = navbar.style.left === '0px';
+        const isMenuToggle = event.target === menuToggle;
+        const isNavbarContent = navbar.contains(event.target);
+
+        if (isOpen && !isMenuToggle && !isNavbarContent) {
+            navbar.style.left = '-300px';
+        }
+    });
 
     menuToggle.addEventListener('click', function () {
         const isOpen = navbar.style.left === '0px';
         navbar.style.left = isOpen ? '-300px' : '0';
-    });
-
-    darkModeSwitch.addEventListener('change', function () {
-        document.body.classList.toggle('dark-mode', darkModeSwitch.checked);
     });
 });
