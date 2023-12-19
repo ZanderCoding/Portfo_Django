9511470faa5a4c04da37da19b65f76bb7db54ea0
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-12-19 14:23:51 -0800

    Changed setting.py

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uu(1f2t#qs(ec6bk1#5xi71+4*c7iwqnu=l9s-awas3(#&#&vn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
