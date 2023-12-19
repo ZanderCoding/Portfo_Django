03878392be5425337cf42b331c928e986434961d
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-12-19 14:41:05 -0800

    hid security key

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uu(1f2t#qs(ec6bk1#5xi71+4*c7iwqnu=l9s-awas3(#&#&vn'
+SECRET_KEY = 'Vercel_Security_Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
