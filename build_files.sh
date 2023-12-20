c381f08a78b055209a3793a8c1dd30a528ac4f3c
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-12-19 17:15:05 -0800

    Changed build command

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "BUILD START"
-python3.12.0 -m pip install -r requirements.txt
-python3.12.0 manage.py collectstatic --noinput --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
