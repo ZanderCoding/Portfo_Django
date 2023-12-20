09a5c5473921944c1469cd665415574508d4fe1a
Alexander Swearingen <alexander.swearingen24@gmail.com>
2023-12-19 17:32:59 -0800

    added static root

--- config/settings.py
+++ config/settings.py
@@ -129,6 +129,8 @@ STATICFILES_DIRS = [
     # Add other app static directories if needed
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
